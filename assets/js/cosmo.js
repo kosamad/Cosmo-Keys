070f47bb18299cfaee9853281d770a37e296eca3
Karen <karenlsamad@gmail.com>
2024-01-29 14:05:40 +0000

    FUNC: timer countdown function which stops at zero

--- assets/js/cosmo.js
+++ assets/js/cosmo.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 
-let game {
-    score: 0,
-    currentGame: [],
-    playerMoves:[],
-    }
+//let game {
+    //score: 0,
+    //currentGame: [],
+    //playerMoves:[],
+    //}
 
 
 // random word generator = wordnik API
@@ -21,34 +21,91 @@ let game {
 //click event listner to start the game
 
 //level 1 play
-
+//js
 //FUNC = new function
 
 // function that slects which level function to impletment using if/if else.
 
-// listening for when the play button is clicked to start the game
-document.querySelector("#start-game").addEventListener('click', startGame);
+//event that listens for when the start button is clicked and toggles the displayed divs
+const startHidden = document.querySelector('.start-hidden');
+const clickHidden = document.querySelector('.click-hidden');
 
-// start game function
-function startGame (){
+function showGameArea(){    
+    startHidden.classList.toggle('hide');
+    clickHidden.classList.toggle('hide');
 
+    //call function to start the timer
+    
+    startTimer();
 
-//level 1 play
-const array1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-function getNextLetter(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const letter = arr[randomIndex];
-    return letter;
 }
 
-const result = getNextLetter(array1);
-document.getElementById("quiz-letter").innerHTML = result
 
-console.log(result);
+//timer function
+function startTimer(){
+
+let startTime = 3;
+    const timerT = document.querySelector('#timer');
+    timerT.innerHTML = `${startTime}`;
+
+    const countDown = setInterval(() => {
+        startTime--;
+        timerT.innerHTML = `${startTime}`;
+        if(startTime === 0){
+            clearInterval (countDown);
+        }
+},1000);
+
+}
+
+
+
+// function to change colour of background as time running 
+//const timerColor = document.getElementById('timer');
+
+//if(remainingTime < 50){
+   // timerColor.style.display = 'figure out how to do tis'
+    
+//}
+// listening for when the play button is clicked to start the game //
+
+//document.querySelector("#start-game").addEventListener('click', startGame); 
+
+// start game function //
+
+//function startGame (){
+
+    //game.score = 0;
+    //game.playerMoves =[];
+    //document.getElementById('player-answer').focus()
+
+
+
+
+//if (getElementById('level-indicator').innerHTML === 'Level 1'){
+//level 1 play//
+//const array1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+//function getNextLetter(arr) {
+    //const randomIndex = Math.floor(Math.random() * arr.length);
+    //const letter = arr[randomIndex];
+    //return letter;
+//}
+
+//const result = getNextLetter(array1);
+//document.getElementById("quiz-letter").innerHTML = result
+
+//console.log(result);
+//}
+
 
 //level 2 play
 
-}
+//}
 
-function playerResponse
+//function playerResponse {}
+
+//function nextTurn () {
+    //game.playerMoves = [];
+
+//}
