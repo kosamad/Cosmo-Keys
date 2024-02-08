db66a0543288c7a25055363e89e8ee519d72623d
Karen <karenlsamad@gmail.com>
2024-02-08 11:23:10 +0000

    reorder of some code

--- assets/js/cosmo.test.js
+++ assets/js/cosmo.test.js
@@ -3,12 +3,3 @@
  * @jest-environment jsdom
  */
 
-describe("newGame works correctly",() =>{
-    beforeAll(() => {
-        game.score = 10;
-        startGame();
-    });
-   test('game score is set to zero', () => {
-    expect(game.score).toEqyal(0);
-   })
-});
