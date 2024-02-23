28c3bdd0b606401d84665e854e790cca582b0d4e
kosamad samad <KarenLSamad@gmail.com>
2024-02-23 17:12:38 +0000

    clarification of comments and varibles moved to aid readability

--- assets/js/cosmo-game.js
+++ assets/js/cosmo-game.js
@@ -1,11 +1,5 @@
-// setTimeout(function () {
-// 	// Hide the loading screen
-// 	document.getElementById('loading-screen').style.display = 'none';
 
-// 	// Show the game content
-// 	document.getElementById('game-content').style.display = 'block';
-// }, 3000); // Adjust the delay as needed
-
+//setting the level indicator for the user
 //takes the stored selected level key (from the level page) and uses it to set the corresponsing level id
 const levelId = localStorage.getItem("selectedLevel");
 const levelIndicator = document.getElementById("level-indicator");
@@ -20,108 +14,7 @@ if (levelId === "level-1") {
 	levelIndicator.style.color = "#e72b2b";
 }
 
-
-
-if ("speechSynthesis" in window) {
-	//Web Speech API is supported
-	//function which speaks the score at the end of the game
-	function speakScore(score) {
-		if (scoreDisplay.innerText === 1) {
-			let scoreVoice = new SpeechSynthesisUtterance(
-				`congratulations, you got ${scoreDisplay.innerText} point.`
-			);
-			window.speechSynthesis.speak(scoreVoice);
-		} else if (scoreDisplay.innerText === "0") {
-			let scoreVoice = new SpeechSynthesisUtterance(
-				`Oh dear, you didn't get any points.`
-			);
-			window.speechSynthesis.speak(scoreVoice);
-		} else {
-			let scoreVoice = new SpeechSynthesisUtterance(
-				`congratulations, you got ${scoreDisplay.innerText} points.`
-			);
-			window.speechSynthesis.speak(scoreVoice);
-		}
-	}
-}
-
-//function which speaks the letter(level-1 play) selected
-function speakLetter(letter) {
-	if ("speechSynthesis" in window) {
-		// Web Speech API is supported
-		if (levelId === "level-3") {
-			let levelThreeWord = new SpeechSynthesisUtterance(currentLetter.innerText);
-			levelThreeWord.rate = 1;
-			window.speechSynthesis.speak(levelThreeWord);
-		} else {
-			for (const char of letter) {
-				let charCheck = char;
-				if (char.toLowerCase() === "a") {
-					charCheck = "ay";
-				}
-				const charVoice = new SpeechSynthesisUtterance(charCheck);
-				charVoice.rate = 1.5; //make it read faster than the default 1 (too slow)
-				window.speechSynthesis.speak(charVoice);
-			}
-		}
-	} else {
-		// Web Speech API is not supported
-		console.log("Web Speech API is not supported in this browser.");
-	}
-}
-
-//timer function
-//variables//
-let startTime = 60;
-let remainingTime = startTime;
-let countDown;
-const timerT = document.querySelector("#timer");
-const timerColor = document.querySelector(".time-container");
-let paused = false;
-
-function startTimer() {
-	timerColor.style.backgroundImage =
-		"radial-gradient(circle, #d0fdcc, #b1fdaa, #91fb85, #6bf95c, #30f61e)";
-	timerT.innerHTML = `${remainingTime}`;
-	countDown = setInterval(() => {
-		if (!paused) {
-			remainingTime--;
-			timerT.innerHTML = `${remainingTime}`;
-
-			//if statements to change the colour of the time-container as time reduces
-			if (remainingTime <= 50 && remainingTime >= 41) {
-				timerColor.style.backgroundImage =
-					"radial-gradient(circle, #e5f6c2, #d2ee9c, #bee676, #a8de4c, #90d600)";
-			} else if (remainingTime <= 40 && remainingTime >= 31) {
-				timerColor.style.backgroundImage =
-					"radial-gradient(circle, #fdf9b2, #f9f392, #f5ed71, #f0e74c, #ece10f)";
-			} else if (remainingTime <= 30 && remainingTime >= 21) {
-				timerColor.style.backgroundImage =
-					"radial-gradient(circle, #f3d7a5, #f2c983, #f1ba61, #f0ab3e, #ef9b09)";
-			} else if (remainingTime <= 20 && remainingTime >= 11) {
-				timerColor.style.backgroundImage =
-					"radial-gradient(circle, #f8c7a8, #f8ae80, #f6945a, #f27934, #ec5b00)";
-			} else if (remainingTime <= 10 && remainingTime >= 0) {
-				timerColor.style.backgroundImage =
-					"radial-gradient(circle, #e97070, #ef5d5a, #f24943, #f33028, #f10000)";
-			}
-			if (remainingTime === 0) {
-				speechSynthesis.cancel();
-				endGameSound.play();
-				clearInterval(countDown);
-				gameRunning = false;
-				message.innerHTML = "Game Over";
-				playerAnswer.contentEditable = "false";
-				setTimeout(() => {
-					speakScore(score);
-				}, 2500);
-			}
-		}
-	}, 1000);
-}
-
-// start game function //
-//variables//
+//  game play variables //
 let score = 0;
 let gameRunning;
 let currentLetter = document.getElementById("quiz-letter");
@@ -133,8 +26,11 @@ let compTurn = true;
 let twoLetterWords;
 let threeLetterWords;
 let lettersTyped = 0;
+let play;
+let pauseBtn;
+let removeIcon;
 
-//array for level 1 play
+//array for level 1 play as API not used
 const letters = [
 	"a",
 	"b",
@@ -164,6 +60,14 @@ const letters = [
 	"z",
 ];
 
+//timer variables
+let startTime = 60;
+let remainingTime = startTime;
+let countDown;
+const timerT = document.querySelector("#timer");
+const timerColor = document.querySelector(".time-container");
+let paused = false;
+
 //sound variables
 let correctSound = new Audio(
 	"assets/sounds/335908__littlerainyseasons__correct.mp3"
@@ -173,6 +77,103 @@ let wrongSound = new Audio("assets/sounds/554053__gronkjaer__wronganswer.mp3");
 wrongSound.volume = 0.5;
 let endGameSound = new Audio("assets/sounds/527650__fupicat__winsquare.wav");
 endGameSound.volume = 0.5;
+
+//speech/sound functions
+
+//function to speak the score at end of the game
+if ("speechSynthesis" in window) {
+	//Web Speech API is supported
+	//function which speaks the score at the end of the game
+	function speakScore(score) {
+		if (scoreDisplay.innerText === 1) {
+			let scoreVoice = new SpeechSynthesisUtterance(
+				`congratulations, you got ${scoreDisplay.innerText} point.`
+			);
+			window.speechSynthesis.speak(scoreVoice);
+		} else if (scoreDisplay.innerText === "0") {
+			let scoreVoice = new SpeechSynthesisUtterance(
+				`Oh dear, you didn't get any points.`
+			);
+			window.speechSynthesis.speak(scoreVoice);
+		} else {
+			let scoreVoice = new SpeechSynthesisUtterance(
+				`congratulations, you got ${scoreDisplay.innerText} points.`
+			);
+			window.speechSynthesis.speak(scoreVoice);
+		}
+	}
+}
+
+//function which speaks the letter (level-1/2 play) or word (level-3 play)
+function speakLetter(letter) {
+	if ("speechSynthesis" in window) {
+		// Web Speech API is supported
+		if (levelId === "level-3") {
+			let levelThreeWord = new SpeechSynthesisUtterance(currentLetter.innerText);
+			levelThreeWord.rate = 1;
+			window.speechSynthesis.speak(levelThreeWord);
+		} else {
+			for (const char of letter) { //separates the characters which is important for level-2 play
+				let charCheck = char;
+				if (char.toLowerCase() === "a") {
+					charCheck = "ay"; //required as "a" sound is not pronouced like the alphabetical "ay"
+				}
+				const charVoice = new SpeechSynthesisUtterance(charCheck);
+				charVoice.rate = 1.5; //make it read faster than the default 1 is too slow for single letter speak
+				window.speechSynthesis.speak(charVoice);
+			}
+		}
+	} else {
+		// Web Speech API is not supported
+		console.log("Web Speech API is not supported in this browser.");
+	}
+}
+
+
+
+//timer functions
+function startTimer() {
+	timerColor.style.backgroundImage =
+		"radial-gradient(circle, #d0fdcc, #b1fdaa, #91fb85, #6bf95c, #30f61e)";
+	timerT.innerHTML = `${remainingTime}`; //time display set to remaining time
+	countDown = setInterval(() => {
+		if (!paused) { //function only runs if the game is  not paused
+			remainingTime--;
+			timerT.innerHTML = `${remainingTime}`;
+			//if statements to change the colour of the time-container as time reduces
+			if (remainingTime <= 50 && remainingTime >= 41) {
+				timerColor.style.backgroundImage =
+					"radial-gradient(circle, #e5f6c2, #d2ee9c, #bee676, #a8de4c, #90d600)";
+			} else if (remainingTime <= 40 && remainingTime >= 31) {
+				timerColor.style.backgroundImage =
+					"radial-gradient(circle, #fdf9b2, #f9f392, #f5ed71, #f0e74c, #ece10f)";
+			} else if (remainingTime <= 30 && remainingTime >= 21) {
+				timerColor.style.backgroundImage =
+					"radial-gradient(circle, #f3d7a5, #f2c983, #f1ba61, #f0ab3e, #ef9b09)";
+			} else if (remainingTime <= 20 && remainingTime >= 11) {
+				timerColor.style.backgroundImage =
+					"radial-gradient(circle, #f8c7a8, #f8ae80, #f6945a, #f27934, #ec5b00)";
+			} else if (remainingTime <= 10 && remainingTime >= 0) {
+				timerColor.style.backgroundImage =
+					"radial-gradient(circle, #e97070, #ef5d5a, #f24943, #f33028, #f10000)";
+			}
+			//when remaining time is 0 the game is over. The timer stops, the end game sound runs and the score is declared.
+			if (remainingTime === 0) {
+				speechSynthesis.cancel();
+				endGameSound.play();
+				clearInterval(countDown);
+				gameRunning = false;
+				message.innerHTML = "Game Over";
+				playerAnswer.contentEditable = "false";
+				setTimeout(() => {
+					speakScore(score);
+				}, 2500);
+			}
+		}
+	}, 1000);
+}
+
+
 
 //level 1 play - load word from array //
 //if (getElementById('level-indicator').innerHTML === 'Level 1'){
@@ -388,9 +389,6 @@ function matchCheck() {
 }
 
 
-let play;
-let pauseBtn;
-let removeIcon;
 
 document.addEventListener("DOMContentLoaded", function () {
 	play = document.getElementById("start-game-button");
@@ -405,27 +403,33 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	play.addEventListener("click", function () {
+
+		//starts the game while also changing the visible box container to a pause/play button
 		var startContainer = document.getElementById("start-box-container");
 		var reloadContainer = document.getElementById("reload-box-container");
-
 		startContainer.classList.add("hide");
 		reloadContainer.classList.remove("hide");
-
-
 		pauseBtn = document.getElementById("pause-btn");
 		removeIcon = document.getElementById("pause-remove");
-
 		pauseBtn.addEventListener("click", togglePause);
 
+		//colour change of pause/play button container only changes when the button is hovered over and returns to normal when the mouse is removed. 
+		pauseBtn.addEventListener('mouseover', function () {
+			document.getElementById("reload-box-container").style.backgroundImage = "radial-gradient(	circle,	#6ee4ef,	#6ee4ef,	#6ee4ef,#6ee4ef,#6ee4ef	)";
+		});
+		pauseBtn.addEventListener('mouseout', function () {
+			document.getElementById("reload-box-container").style.backgroundImage = "linear-gradient(	to right top,#ea505a,#f2624a,#f57639,#f28b27,#eba012)";
+		});
+
+		//game and timer functions are called to start the game
 		startGame();
 		startTimer();
 	});
 });
 
-
+//ensure cursor moves to the end of the prev typed letter (and not to the start)
 function focusAtEnd() {
 	let reFocus = document.querySelector(".focus-point");
-	//cursor back inside the player answer box and moved to the end of the text already entered
 	reFocus.focus();
 	let range = document.createRange();
 	range.selectNodeContents(reFocus);
@@ -435,9 +439,13 @@ function focusAtEnd() {
 	selection.addRange(range);
 }
 
+
+//adding the event listener again after the initial click has been done to start the game
 document.getElementById("pause-btn").addEventListener("click", togglePause);
+
+
+//changes pause/play icon depending on which is showing if the game is running 
 function togglePause() {
-	console.log("i've been clicked")
 	if (gameRunning) {
 		if (removeIcon.classList.contains("fa-pause")) {
 			removeIcon.classList.remove("fa-pause");
