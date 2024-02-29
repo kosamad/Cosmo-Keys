4422c755ad1b6516093cbdbd1cc23797f1474325
kosamad samad <KarenLSamad@gmail.com>
2024-02-29 09:35:34 +0000

    FEAT: Play button colour re-set when using the back button

--- assets/js/cosmo-index.js
+++ assets/js/cosmo-index.js
@@ -1,6 +1,9 @@
 
 
-indexPlayBtn = document.getElementById("index-play-btn");
+let indexPlayBtn = document.getElementById("index-play-btn");
+let restartColour = document.getElementById("index-play-outer");
+
+restartColour.style.backgroundImage = "linear-gradient(to right top, #e597f6,  #d27ff0, #bd67eb,  #a64fe7, #8a38e3)";
 
 		
 //colour change of button containers only changes when the button is hovered over and returns to normal when the mouse is removed. 
